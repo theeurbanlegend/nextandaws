810f13b44d29d390ad5913e07b7192c1c5ea51bf
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2024-02-03 17:33:42 +0300

    Upload to s3

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import UploadForm from "./(form)/form";
+
+export default function Page(){
+    return (
+        <>
+        <h1>Upload files to s3 bucket.</h1>
+        <UploadForm/>
+        </>
+    )
+}
